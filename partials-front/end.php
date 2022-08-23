e557f4248549131a9d1034db8655c560d3b5c72f
shifatz <108052841+shifatz@users.noreply.github.com>
2022-08-23 12:12:52 +0600

    Update end.php

--- partials-front/end.php
+++ partials-front/end.php
@@ -1,7 +1,7 @@
 <!-- Section Starts Here -->
 <section class="Dead_end">
         <div class="Container text-right delete_border"> 
-            <p>Developed By <a href="https://youtu.be/dQw4w9WgXcQ">Showrav Zaman</a></p>
+            <p>Developed By <a href="https://youtu.be/dQw4w9WgXcQ">Mehbub Shifat Ur Rasul</a></p>
         </div>
     </section>
     <!-- Section Ends Here -->
