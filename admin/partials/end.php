b908592c4514934923d8e4417f91ad5cf77b6a85
shifatz <108052841+shifatz@users.noreply.github.com>
2022-08-23 12:15:46 +0600

    Update end.php

--- admin/partials/end.php
+++ admin/partials/end.php
@@ -1,7 +1,7 @@
 <!-- Section Starts Here -->
 <div>
             <div class="wrapper footer text-right">
-            <p>Developed By <a href="https://youtu.be/dQw4w9WgXcQ">Showrav Zaman</a></p>
+            <p>Developed By <a href="https://youtu.be/dQw4w9WgXcQ">Mehbub Shifat Ur Rasul</a></p>
             </div>
         </div>
         <!-- Section End Here -->
